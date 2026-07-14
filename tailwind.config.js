276c39fa09263edb98d22d05b63cbd31e38b4f12
ganetika <em.gantan@gmail.com>
2026-07-14 22:06:34 +0700

    build: compile Tailwind CSS static build, add package.json and config, remove CDN scripts

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./about/**/*.html",
+    "./contact/**/*.html",
+    "./articles/**/*.html",
+    "./terms/**/*.html",
+    "./privacy/**/*.html",
+    "./disclosures/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
